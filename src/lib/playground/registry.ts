23e952afebd7088dee0899b790c8e1a5fd52f5f4
Gary <gary@revrebel.io>
2026-05-13 03:39:49 -0700

    Commit

--- src/lib/playground/registry.ts
+++ src/lib/playground/registry.ts
@@ -246,6 +246,12 @@ export const PLAYGROUND_REGISTRY: Record<string, any> = {
     component: dynamic(() => import('@/widgets/TimelineComponent/index').then(mod => (mod as any).default || (mod as any)[Object.keys(mod)[0]])),
     metadata: null
   },
+  widget_TopChannelsChart: { 
+    name: "TopChannelsChart", 
+    type: "widget",
+    component: dynamic(() => import('@/widgets/TopChannelsChart/index').then(mod => (mod as any).default || (mod as any)[Object.keys(mod)[0]])),
+    metadata: null
+  },
   widget_TopServicesBarChart: { 
     name: "TopServicesBarChart", 
     type: "widget",
@@ -275,6 +281,12 @@ export const PLAYGROUND_REGISTRY: Record<string, any> = {
     type: "widget",
     component: dynamic(() => import('@/widgets/YearMonthSelector/index').then(mod => (mod as any).default || (mod as any)[Object.keys(mod)[0]])),
     metadata: widget_YearMonthSelector_metadata
+  },
+  widget__shared: { 
+    name: "_shared", 
+    type: "widget",
+    component: dynamic(() => import('@/widgets/_shared/index').then(mod => (mod as any).default || (mod as any)[Object.keys(mod)[0]])),
+    metadata: null
   },
   widget_metric_cards: { 
     name: "metric-cards", 
